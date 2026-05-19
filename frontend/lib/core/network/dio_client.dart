e4fdf10e75641af2e9dd3fe8ca4e60da57e18e3c
Nishant Sharma <nishant@Nishants-MacBook-Air-65.local>
2026-05-19 15:21:59 +0530

    Changed backend url

--- frontend/lib/core/network/dio_client.dart
+++ frontend/lib/core/network/dio_client.dart
@@ -9,7 +9,7 @@ class DioClient {
   DioClient(SecureStorage storage, {Future<void> Function()? onUnauthorized}) {
     dio = Dio(
       BaseOptions(
-        baseUrl: 'http://10.129.7.48:9000',
+        baseUrl: 'http://103.21.126.116:9000',
         connectTimeout: const Duration(seconds: 5),
         receiveTimeout: const Duration(seconds: 5),
         headers: {'Content-Type': 'application/json'},
